9616114c5efb6f6ea8d583382f52a9c00f05d6f9
Bailey <bailey.g.weathers@gmail.com>
2018-09-02 23:09:20 -0500

    boostarp sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_firstName
+  before_create :set_lastName
+
+  private
+
+  def set_firstName
+    self.firstName.titeize
+  end
+
+  def set_lastName
+    self.lastName.titeize
+  end
 end
