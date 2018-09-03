359bafadbe64a9b161eedade92d4fca5a51bd5f0
Bailey <bailey.g.weathers@gmail.com>
2018-09-02 23:57:47 -0500

    Update devise before_action in app controller to allow firstName and lastName

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  before_create :set_firstName
-  before_create :set_lastName
-
-  private
-
-  def set_firstName
-    self.firstName.titeize
-  end
-
-  def set_lastName
-    self.lastName.titeize
-  end
 end
