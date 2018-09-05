4b1110ade7105afdc8eaa7f04025b3b38ec85960
Bailey <bailey.g.weathers@gmail.com>
2018-09-05 14:29:54 -0500

    Complete User views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
   get 'faq' => 'welcome#faq'
+
+  devise_for :users
+
 
   root 'welcome#index'
 end
