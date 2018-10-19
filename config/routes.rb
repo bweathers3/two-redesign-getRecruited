b079e82a26ecd63dd0cc963cc283a81a88ea0dfe
Bailey <bailey.g.weathers@gmail.com>
2018-10-19 07:23:55 -0300

    Complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,20 @@ Rails.application.routes.draw do
     resources :sports, except: [:index]
   end
 
+  resources :athletes do
+    resources :academic
+    resources :address
+    resources :contact
+    resources :counselor_contact
+    resources :current_address
+    resources :father_address
+    resources :father_contact
+    resources :mother_address
+    resources :mother_contact
+    resources :school_address
+    resources :sibling
+  end
+
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
   get 'faq' => 'welcome#faq'
